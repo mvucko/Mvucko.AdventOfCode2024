2e86bab9bef6e6fdf7df7d0c913744ea9a39e246
misla <misla@UWULAPTOP>
2024-12-06 20:33:49 +0100

    Add project files.

---
+++ Mvucko.AdventOfCode2024/ISolution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mvucko.AdventOfCode2024
+{
+    internal interface ISolution
+    {
+        public int TaskNumber { get; }
+        public void Solve();
+    }
+}
---
+++ Mvucko.AdventOfCode2024/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Mvucko.AdventOfCode2024;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Mislav's AOC 2024!");
+        Console.WriteLine("X for exit at any input!");
+
+        bool exit = false;
+        while (!exit)
+        {
+            Console.WriteLine("Input day to solve: ");
+            var input = Console.ReadLine();
+            if (input.ToUpper() == "X")
+            {
+                exit = true;
+            }
+
+            Console.WriteLine("Get result for task number: ");
+            var key = Console.ReadLine();
+            try
+            {
+                var number = Convert.ToInt32(input);
+                Solve(number);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("not a number :)");
+            }
+        }
+    }
+
+
+    private static void Solve(int input)
+    {
+        var handlers = AppDomain.CurrentDomain.GetAssemblies()
+                       .SelectMany(s => s.GetTypes())
+                       .Where(p => typeof(ISolution).IsAssignableFrom(p) && p.IsClass);
+        foreach (var handler in handlers)
+        {
+            var handlerInstance = (ISolution)Activator.CreateInstance(handler);
+            if (handlerInstance.TaskNumber == input)
+            {
+                handlerInstance.Solve();
+            }
+        }
+
+    }
+}
